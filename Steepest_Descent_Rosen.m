e21c7dd18d6decda3262b998df76a0466b8f2693
khyar <zouheir.khyar@etu.unistra.fr>
2026-04-08 13:15:18 +0200

    fixed steepest descent rosen

--- Steepest_Descent_Rosen.m
+++ Steepest_Descent_Rosen.m
@@ -4,21 +4,36 @@ x = x0;
 x = x'
 X=[x]
 n = 0
+
+%% the main loop
 while true
+    % get the corresponding gradient with x
     g = Grad_Rosenbrock(x);
-    %d = -g
+    
+    % init our alpha to 0.1 as was asked
     alpha = 0.1;
+
+    % calculation of alpha with newtons method
     while true
+
+        % value of gradient with x - alpha*g
         phi1 = grad_phi(x, g, alpha);
+        
+        % value of hessian with x - alpha*g
         phi2 = grad2_phi(x, g, alpha);
+
+        % ak+1 = ak - f'(x-a*g)/f''(x-a*g);
         alpha_new = alpha - phi1/phi2;
-        if abs(alpha_new-alpha)/max(1, abs(alpha)) < eps2
+        if abs(alpha_new-alpha)/max(1, alpha) < eps2
             break;
         end
         alpha = alpha_new;
     end
+
+    % xk+1 = x - a*g
     x_new = x - alpha * g;
 
+    % check if the stopping criteria was reached
     r = norm(x_new-x);
     if (norm(x)<=0)
         r = 0;
@@ -29,11 +44,16 @@ while true
     if r < eps1
         break;
     end
+
+    % update number of steps
     n = n + 1;
+
+    % update x and the new x to the set of points visited
     x=x_new;
     X = [X x];
 end
 
+% draw the convergence
 figure; hold on; grid on;
 plot(X(1,:), X(2,:), 'o-', 'LineWidth', 1.5);
 xlabel('x1');
